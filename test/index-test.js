1f0b3b198de53d19fab10b0a31deff795efaac88
racie7 <graciamuts@gmail.com>
2022-07-31 20:33:15 +0300

    sshtest

--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,6 @@ chai.use( require( 'chai-fs' ) );
 
 describe( 'This assignment', () => {
   it( 'has been correctly cloned to your local environment', () => {
-    chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
+    chai.assert.isDirectory( './.git', 'no ".git" folder(s) was found within "welcome-completing-assignment". Use "git init" to create one' );
   } )
 } )
